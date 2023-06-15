b2165a06569f1b8225643f3d1c8efa616e3a0886
Roman Boyko <112849174+RomanVillain@users.noreply.github.com>
2023-06-15 18:58:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/")
-def index():
-    return render_template("style.css")
-
 if __name__ == "__main__":
     app.run()
 
-if __name__ == "__main__":
-    app.run()
